576dc262d05484deb3ee4b188da1f6c19f61d36e
Dennis Choi <dennis.choi7@gmail.com>
2016-04-21 01:22:50 -0400

    JS array, individually going through one at a time through console log

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -5,3 +5,20 @@
 // Create an array to hold your top choices (colors, presidents, whatever).
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
+
+
+var veggies = ['Carrot', 'Cucumber', 'Tomato', 'Zucchini']
+console.log('My #1 Veggie is ' + veggies[0] + '.')
+My #1 Veggie is Carrot.
+console.log('My #1 Veggie is ' + veggies[1] + '.')
+My #1 Veggie is Cucumber.
+console.log('My #1 Veggie is ' + veggies[2] + '.')
+My #1 Veggie is Tomato.
+console.log('My #1 Veggie is ' + veggies[3] + '.')
+My #1 Veggie is Zucchini.
+
+
+
+Bonus:
+console.log('My #1 Veggie is ' + veggies[0] + ', My 2nd choice is ' + veggies[1] + ', 3rd is ' + veggies[2] + ', Last but not least... ' + veggies[3] + '.')
+  ==> My #1 Veggie is Carrot, My 2nd choice is Cucumber, 3rd is Tomato, Last but not least... Zucchini.
